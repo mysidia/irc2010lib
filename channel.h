b0255bfcdbc5c9825eab1157625a13dc4dcbe288
Mysidia <69487+mysidia@users.noreply.github.com>
2001-10-25 21:21:03 +0000

    *** empty log message ***

--- channel.h
+++ channel.h
@@ -2,5 +2,14 @@ struct _chanmode
 {
 	long mode[2];
 };
-
 typedef struct _chanmode IrcChannelMode;
+
+struct _chanhandle
+{
+	char		*channelName;
+	time_t		created;
+	time_t		lastTime;
+	IrcChannelMode	mode;
+	LIST_HEAD(,_chanuser)	*users;
+};
+typedef struct _chanhandle IrcChannel;
